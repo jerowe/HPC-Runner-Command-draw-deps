16b4cd4e454ecf7621268478a8ae56885644ac18
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-01-10 11:33:25 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+HPC::Runner::Command::draw\_deps - Blah blah blah
+
+# SYNOPSIS
+
+    use HPC::Runner::Command::draw_deps;
+
+# DESCRIPTION
+
+HPC::Runner::Command::draw\_deps is
+
+# AUTHOR
+
+Jillian Rowe <jillian.e.rowe@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2017- Jillian Rowe
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/HPC/Runner/Command/draw_deps.pm
@@ -0,0 +1,39 @@
+package HPC::Runner::Command::draw_deps;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+HPC::Runner::Command::draw_deps - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use HPC::Runner::Command::draw_deps;
+
+=head1 DESCRIPTION
+
+HPC::Runner::Command::draw_deps is
+
+=head1 AUTHOR
+
+Jillian Rowe E<lt>jillian.e.rowe@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2017- Jillian Rowe
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
